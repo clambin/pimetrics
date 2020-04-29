0bb4eba79b799ba14e307a2e1faff23815ab4eb8
Christophe Lambin <christophe.lambin@gmail.com>
2020-04-29 14:25:30 +0200

    Moved to separate library

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pymetrics',
+      version='1.0.0',
+      description='Library of probes to measure metrics & report to a monitoring system (e.g. Prometheus)',
+      author='Christophe Lambin',
+      author_email='christophe.lambin@gmail.com',
+      url='https://github.com/clambin/pymetrics',
+      packages=['pymetrics'],
+      )
