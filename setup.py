4de008729917821e5ac833ffa4bf65ee6b66467e
Christophe Lambin <christophe.lambin@gmail.com>
2020-04-29 18:58:08 +0200

    first version to pypi

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 import setuptools
+from pimetrics.version import version
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pimetrics",
-    version="0.1.0",
+    name='pimetrics',
+    version=f'{version}',
     author='Christophe Lambin',
     author_email='christophe.lambin@gmail.com',
-    description="Metrics to measure data from different sources and report to a monitoring system (e.g. Prometheus)",
+    description='Metrics to measure data from different sources and report to a monitoring system (e.g. Prometheus)',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     url='https://github.com/clambin/pimetrics',
-    packages=['pimetrics'], # setuptools.find_packages(),
+    packages=['pimetrics'],  # setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.7')
