5c3e28d803b39458fccee8e966856eb3ef65e829
Christophe Lambin <christophe.lambin@gmail.com>
2020-05-15 21:24:33 +0200

    Add sast + move to rules:

--- pimetrics/probe.py
+++ pimetrics/probe.py
@@ -3,7 +3,7 @@
 
 import queue
 import shlex
-import subprocess # nosec
+import subprocess  # nosec
 import threading
 import requests
 from abc import ABC, abstractmethod
@@ -68,7 +68,7 @@ class FileProbe(Probe):
 class ProcessReader:
     def __init__(self, cmd):
         self.cmd = cmd
-        self.process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, encoding='utf-8') # nosec
+        self.process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, encoding='utf-8')  # nosec
         self.queue = queue.Queue()
         self.thread = threading.Thread(target=self._enqueue_output)
         self.thread.daemon = True
@@ -111,7 +111,8 @@ class ProcessProbe(Probe, ABC):
         # process may not have any data to measure
         while output is None:
             lines = []
-            for line in self.reader.read(): lines.append(line)
+            for line in self.reader.read():
+                lines.append(line)
             output = lines
         return output
 
